72f2d8cf5e8e9bc50a249e17a748635785a7b847
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2025-12-17 12:16:09 +0100

    Add lego offer to wazuh for dashboards to consume (#18)

--- deployments/wazuh/ps6/outputs.tf
+++ deployments/wazuh/ps6/outputs.tf
@@ -2,3 +2,9 @@ output "s3_creds" {
   value     = openstack_identity_ec2_credential_v3.wazuh_indexer_s3_creds
   sensitive = true
 }
+
+output "lego_provides" {
+  value = {
+    certificates = "certificates"
+  }
+}
